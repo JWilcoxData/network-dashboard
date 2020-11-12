ffa04396de8c9d15f5cf77a23e1ebc2eda72dc81
JWilcoxData <jason@yourdatastory.io>
2020-11-11 18:44:21 -0700

    Applied Styling using Styler for testing.

--- worksheet_network-eda.R
+++ worksheet_network-eda.R
@@ -9,18 +9,23 @@ library(dplyr)
 
 
 # Data Loading ----------
-data <- read.csv(file = 'raw_data1.csv',
-         header = TRUE,
-         stringsAsFactors = FALSE
-             )
+data <- read.csv(
+  file = "raw_data1.csv",
+  header = TRUE,
+  stringsAsFactors = FALSE
+)
 
 
 # Data Transformation ------
 
 # Column renaming
-names(data) <- names(data) %>% gsub(pattern = "(\\.)+",
-                                    replacement = "_") %>% tolower()
+names(data) <- names(data) %>%
+  gsub(
+    pattern = "(\\.)+",
+    replacement = "_"
+  ) %>%
+  tolower()
 
 data %>%
-    group_by(source) %>%
-    summarise(total = n())
+  group_by(source) %>%
+  summarise(total = n())
