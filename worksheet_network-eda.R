60dc76550d2eb41dbb1c640990323487aac3b562
Jason <jason@Jasons-MacBook-Pro.local>
2020-11-12 22:08:01 -0700

    Added csv and started working on IP greps

--- worksheet_network-eda.R
+++ worksheet_network-eda.R
@@ -15,6 +15,17 @@ data <- read.csv(
   stringsAsFactors = FALSE
 )
 
+ip_addresses <- read.csv(
+  file = "data/GeoLite2-Country-Blocks-IPv4.csv",
+  header = TRUE,
+  stringsAsFactors = FALSE
+)
+
+locations <- read.csv(
+  file = "data/GeoLite2-Country-Locations-en.csv",
+  header = TRUE,
+  stringsAsFactors = FALSE
+)
 
 # Data Transformation ------
 
@@ -26,6 +37,26 @@ names(data) <- names(data) %>%
   ) %>%
   tolower()
 
+ip_addresses <- ip_addresses %>%
+  mutate(
+    address = gsub(
+      pattern = "(\\/[0-9][0-9])",
+      replacement = "",
+      network
+    )
+  )
+
+ip_locale <- merge(ip_addresses, locations, by.x = "geoname_id", by.y = "geoname_id", all = TRUE) %>%
+  select(
+    address, geoname_id, continent_code, continent_name, country_iso_code, country_name
+  )
+
+temp <- merge(data, ip_addresses, by.x = c("destination"), by.y = c("address"), all.x = TRUE)
+
 data %>%
   group_by(source) %>%
   summarise(total = n())
+
+
+
+
